9646ffbcc3aeec6f075a9e3bdcb4add335c88bf1
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-09 13:59:41 -0700

    Shortened distance inbetween photos and extraneous photos are half opacity

--- public/js/script.js
+++ public/js/script.js
@@ -1,75 +1,4 @@
-// alert("Hello");
-
-// $(".imageFrame").click(function () {
-//   $(this).toggleClass("imageFrame");
-// });
-
-// $(function () {
-//   "use strict";
-//   var view = $(window).height();
-//   $(".part")
-//     .height(view)
-//     .scrollie({
-//       scrollOffset: -50,
-//       scrollingInView: function (elem) {
-//         var bgColor = elem.data("background");
-//         $("body").css("background-color", bgColor);
-//       },
-//     });
-// });
-
-// window.addEventListener("scroll", () => {
-//   const scrollable = document.documentElement.scrollHeight - window.innerHeight;
-//   const scrolled = window.scrollY;
-//   console.log(scrolled);
-//   if (Math.ceil(scrolled) === scrollable) {
-//     alert("Youve reached the bottom!");
-//   }
-// });
-
-var slide = document.querySelector("article");
-
-// Get it's position in the viewport
-var bounding = slide.getBoundingClientRect();
-
-// function isScrolledIntoView(elem) {
-//   var docViewTop = $(window).scrollTop();
-//   var docViewBottom = docViewTop + $(window).height();
-//
-//   var elemTop = $(elem).offset().top;
-//   var elemBottom = elemTop + $(elem).height();
-//
-//   return elemBottom <= docViewBottom && elemTop >= docViewTop;
-// }
-// console.log(bounding);
-// function returning() {
-//   if (
-//     bounding.top >= 0 &&
-//     bounding.left >= 0 &&
-//     bounding.right <=
-//       (window.innerWidth || document.documentElement.clientWidth) &&
-//     bounding.bottom <=
-//       (window.innerHeight || document.documentElement.clientHeight)
-//   )
-//     console.log(this);
-// }
-
-// Log the results
-// console.log(bounding);
-// console.log(slide.dataset.background);
-
-// if (
-//   bounding.top >= 0 &&
-//   bounding.left >= 0 &&
-//   bounding.right <=
-//     (window.innerWidth || document.documentElement.clientWidth) &&
-//   bounding.bottom <=
-//     (window.innerHeight || document.documentElement.clientHeight)
-// ) {
-//   console.log("In the viewport!");
-// } else {
-//   console.log("Not in the viewport... whomp whomp");
-// }
+////////////////////////////////////////////////// BACKGROUND COLOR CHANGE WHEN IMAGE IS IN VIEWPORT
 var intersectionOptions = {
   root: null, // use the viewport
   rootMargin: "0px",
@@ -85,7 +14,6 @@ function intersectionCallback(entries, observer) {
       $("body").css("background-color", boom);
     } else {
       console.log("Not fully visible!");
-      // $("body").css("background-color", originalColor);
     }
   });
 }
@@ -99,32 +27,108 @@ document.querySelectorAll(".imageFrame").forEach((imageFrame) => {
   observer.observe(imageFrame);
 });
 
-var original = document.querySelector("body");
-var originalColor = original.dataset.background;
-// var target = document.querySelector(".imageFrame");
-// // observer.observe(target);
-// var slideBox = document.querySelector(".slide");
-// var specific = slideBox.dataset.background;
-// console.log(specific);
-// var newColor = document.querySelector(".imageFrame");
-// var newSpecific = newColor.dataset.background;
+// var original = document.querySelector("body");
+// var originalColor = original.dataset.background;
 
-//
-// function intersectionCallback(entries, observer) {
-//   entries.forEach((entry) => {
-//     if (entry.intersectionRatio >= 1) {
-//       console.log("Fully visible!");
-//     } else {
-//       console.log(entry.intersectionRatio);
-//       console.log("Not fully visible!");
-//     }
+//////////////////////////////////////////////////images are half opacity when not fully visable
+var intersectionOptionsTwo = {
+  root: null, // use the viewport
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+
+function intersectionCallbackTwo(items, observerTwo) {
+  items.forEach((item) => {
+    if (item.intersectionRatio >= 1) {
+      let fullOpac = 1;
+      let color = item.target.dataset.background;
+      console.log("Fully visible! Full Opacity");
+      console.log(".slide[data-background=" + color + "]");
+      $(".slide[data-background=" + color + "]").css("opacity", fullOpac);
+    } else {
+      console.log("Not fully visible! Half Opacity");
+      var halfOpac = 0.5;
+      let colorTwo = item.target.dataset.background;
+      $(".slide[data-background=" + colorTwo + "]").css("opacity", halfOpac);
+    }
+  });
+}
+
+var observerTwo = new IntersectionObserver(
+  intersectionCallbackTwo,
+  intersectionOptionsTwo
+);
+
+document.querySelectorAll(".imageFrame").forEach((imageFrame) => {
+  observerTwo.observe(imageFrame);
+});
+
+////////////////////////////////////////////////////////////////AMOUNT WRAPPER IS MOVED FROM THE LEFT DEPENDING ON WINDOW SIZE
+var intersectionOptionsThree = {
+  root: null, // use the viewport
+  rootMargin: "0px",
+  threshold: 1,
+};
+
+function intersectionCallbackThree(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio <= 1) {
+      console.log("A tag is visible");
+      let vpWidth = Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+      );
+      let image = $(".slide").css("width");
+      let imageSize = parseInt(image, 10);
+      let halfWidth = (vpWidth - imageSize) / 2;
+      $(".wrapper").css("top", halfWidth);
+    } else {
+      console.log("A tag is not visible");
+    }
+  });
+}
+
+var observerThree = new IntersectionObserver(
+  intersectionCallbackThree,
+  intersectionOptionsThree
+);
+
+document.querySelectorAll("a").forEach((wrapper) => {
+  observerThree.observe(wrapper);
+});
+
+function selectstart(event) {
+  if (dragging) DomEvent.stop(event);
+}
+
+////////////////////////////////////////////////////////////////////////////BACK TO BEGINNING button
+
+// $(document).ready(function () {
+//   // $(window).scroll(function () {
+//   //   if ($(this).scrollTop() > 100) {
+//   //     $(".arrow").fadeIn();
+//   //   } else {
+//   //     $(".arrow").fadeOut();
+//   //   }
+//   // });
+//   // $(".arrow").click(function () {
+//   //   console.log("arrow clicked");
+//   //   $("html, body").animate({ scrollTop: 0 }, 600);
+//   //   return false;
+//   // });
+// });
+
+// $(document).ready(function () {
+//   $("#topBtn").click(function () {
+//     console.log("arrow clicked");
+//     $("html, body").animate({ scrollRight: 0 }, 800);
+//     // return false;
 //   });
-// }
-//
-// var observer = new IntersectionObserver(
-//   intersectionCallback,
-//   intersectionOptions
-// );
-//
-// var target = document.querySelector("#box");
-// observer.observe(target);
+// });
+
+// $(function () {
+//   $("#topBtn").bind("click", function (event) {
+//     $("html, body").animate({ scrollTop: 0 }, 1500);
+//     event.preventDefault();
+//   });
+// });
