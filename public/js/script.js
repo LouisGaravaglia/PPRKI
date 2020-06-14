41a521aa4621237b45a0fe28b1c386a333b38bca
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-06-13 17:08:23 -0700

    added gradient changing bg for gallery page with 16 different hex colors

--- public/js/script.js
+++ public/js/script.js
@@ -27,11 +27,11 @@ document.querySelectorAll(".imageFrame").forEach((imageFrame) => {
   observer.observe(imageFrame);
 });
 
-//////////////////////////////////////////////////images are half opacity when not fully visable
+//////////////////////////////////////////////////images are half opacity when not fully visable,
 var intersectionOptionsTwo = {
   root: null, // use the viewport
   rootMargin: "0px",
-  threshold: 1.0,
+  threshold: 1,
 };
 
 function intersectionCallbackTwo(items, observerTwo) {
@@ -71,6 +71,7 @@ function intersectionCallbackThree(entries, observer) {
   entries.forEach((entry) => {
     if (entry.intersectionRatio <= 1) {
       console.log("A tag is visible");
+
       let vpWidth = Math.max(
         document.documentElement.clientWidth || 0,
         window.innerWidth || 0
@@ -100,7 +101,7 @@ function selectstart(event) {
 
 ////////////////////////////////////////////////////////////////////////////BACK TO BEGINNING button
 
-(function () {
+function arrowScroll() {
   let btn = document.querySelector("#topBtn");
 
   btn.addEventListener(
@@ -115,4 +116,49 @@ function selectstart(event) {
     },
     false
   );
+}
+
+////////////////////////////////////////////////////////////////////////SET CHANGING GRADIENT BACKGROUND COLORS
+var colors = [
+  "#f3d58b",
+  "#f3d59b",
+  "#f3d5ab",
+  "#f3d5bb",
+  "#f3d5cb",
+  "#e3d5cb",
+  "#d3d5cb",
+  "#c3d5cb",
+  "#b3d5cb",
+  "#a3d5cb",
+  "#93d5cb",
+  "#83d5cb",
+  "#73d5cb",
+  "#63d5cb",
+  "#53d5cb",
+  "#63d5cb",
+  "#73d5cb",
+  "#83d5cb",
+  "#93d5cb",
+  "#a3d5cb",
+  "#b3d5cb",
+  "#c3d5cb",
+  "#d3d5cb",
+  "#e3d5cb",
+  "#f3d5cb",
+  "#f3d5bb",
+  "#f3d5ab",
+  "#f3d59b",
+  "#f3d58b",
+];
+let galleryBG = document.querySelector(".videoBox-gallery");
+
+var currentColor = 0;
+(function switchColor() {
+  if (currentColor >= colors.length) currentColor = 0;
+
+  $(".videoBox-gallery").css("background-color", colors[currentColor++]);
+
+  setTimeout(switchColor, 2000);
+
+  console.log(`Gallery background chaning to ${currentColor}`);
 })();
